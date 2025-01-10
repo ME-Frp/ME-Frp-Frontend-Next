2a33e47414b5f012615d839a7ba8cf5db2a24f27
LxHTT <lxhtt@mcsl.com.cn>
2025-01-10 22:38:00 +0800

    fix: beian

--- src/components/HomeFooter.vue
+++ src/components/HomeFooter.vue
@@ -2,8 +2,8 @@
   <footer class="footer">
     <div class="footer-content">
       <p>Copyright © ME Frp 项目组 2021-2024.</p>
-      <p><a href="https://内网穿透.中国/">Frp 内网穿透联盟统一识别编码:AZWB66WB</a></p>
-      <p><a href="https://beian.miit.gov.cn">蜀ICP备2023020251号</a></p>
+      <p><a href="https://内网穿透.中国/">Frp 内网穿透联盟统一识别编码: AZWB66WB</a></p>
+      <p><a href="https://beian.miit.gov.cn">闽 ICP 备 2024079434号-1</a></p>
       <p><a href="https://beian.miit.gov.cn">增值电信业务许可证号: B1-20242579</a></p>
     </div>
   </footer>
