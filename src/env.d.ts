2586e632e4f8848a6588f7e4d338dca2c2bcf258
LxHTT <lxhtt@mcsl.com.cn>
2025-01-11 12:16:46 +0800

    tryfix: build

--- src/env.d.ts
+++ src/env.d.ts
@@ -4,4 +4,12 @@ declare module '*.vue' {
   import type { DefineComponent } from 'vue'
   const component: DefineComponent<{}, {}, any>
   export default component
-} 
+}
+
+interface ImportMetaEnv {
+  readonly VITE_API_ENDPOINT: string
+}
+
+interface ImportMeta {
+  readonly env: ImportMetaEnv
+}
