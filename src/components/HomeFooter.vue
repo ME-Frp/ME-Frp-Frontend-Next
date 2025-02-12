87331ccec86835cddf2eab83592b31189e7a5495
LxHTT <lxhtt@mcsl.com.cn>
2025-02-12 16:53:44 +0800

    remove sth

--- src/components/HomeFooter.vue
+++ src/components/HomeFooter.vue
@@ -4,7 +4,6 @@
       <p>Copyright © ME Frp 项目组 2021-2025.</p>
       <p><a href="https://内网穿透.中国/">Frp 内网穿透联盟统一识别编码: AZWB66WB</a></p>
       <p><a href="https://beian.miit.gov.cn">蜀 ICP 备 2023020251 号</a></p>
-      <p><a href="https://beian.miit.gov.cn">增值电信业务许可证号: B1-20242579</a></p>
     </div>
   </footer>
 </template>
