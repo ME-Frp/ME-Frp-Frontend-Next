0a87f9bedfe791aab19dfab2d553dea52f649b5b
LxHTT <lxhtt@mcsl.com.cn>
2025-04-04 23:39:14 +0800

    bump: version

--- src/views/dashboard/About.vue
+++ src/views/dashboard/About.vue
@@ -4,7 +4,7 @@
       <NSpace vertical :size="24">
         <NDescriptions label-placement="left" bordered :column="1">
           <NDescriptionsItem label="面板版本">
-            v5.3.4
+            v5.4.1
           </NDescriptionsItem>
           <NDescriptionsItem label="开发者">
             <div class="user-info">
